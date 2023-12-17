dfa1de83e605a6ebbdddd26a8bc9c3c38a5cf626
Shivam Middha <shivammiddha2001@gmail.com>
2023-12-17 14:49:44 +0530

    Online-Code-Editor

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div
+            className='main lg:flex md:flex flex-wrap justify-center items-center px-4 bg-[#2f3640] py-4'>
+            <div className="left">
+                <div className="logo font-bold text-2xl text-white text-center">
+                    <h2>Alight-Code-Pen</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
